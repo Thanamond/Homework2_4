d1e695345e966c4cb83211fd17ba4861e3e85e59
Thanamond <122435863+Thanamond@users.noreply.github.com>
2023-05-20 17:23:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        int N = int.Parse(input[0]);
+        int K = int.Parse(input[1]);
+
+        string[] population = Console.ReadLine().Split();
+        int maxPopulation = 0;
+   
+        for (int i = 1; i < N-K; i++)
+        {
+            int sum = 0;
+            sum += int.Parse(population[i]);
+            for (int j = 1; j < K+1; j++)
+            {
+                sum += int.Parse(population[i+j]);
+                sum += int.Parse(population[i-j]);
+            }
+            if (sum > maxPopulation)
+            {
+                maxPopulation = sum;
+            }
+        }
+        Console.WriteLine(maxPopulation);
+    }
+}
